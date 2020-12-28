c3818e283a3284ee1713465cec87e2e8dfc9eca1
Erik R. Neumann <erikn@myPhysicsLab.com>
2020-12-28 10:53:14 -0800

    AffineTransformTest: use const instead of var

--- src/lab/util/test/AffineTransformTest.js
+++ src/lab/util/test/AffineTransformTest.js
@@ -37,65 +37,63 @@ static test() {
 static testAffineTransform() {
   startTest(AffineTransformTest.groupName+'testAffineTransform');
 
-  var tol = 1E-13;
+  const tol = 1E-13;
 
   // identity
-  var at = AffineTransform.IDENTITY;
-  assertTrue(at.transform(Vector.ORIGIN).equals(Vector.ORIGIN));
+  const a1 = AffineTransform.IDENTITY;
+  assertTrue(a1.transform(Vector.ORIGIN).equals(Vector.ORIGIN));
 
   // translate
-  at = AffineTransform.IDENTITY.translate(2, 3);
-  assertTrue(at.transform(0, 0).nearEqual(new Vector(2, 3), tol));
-  assertTrue(at.transform(Vector.ORIGIN).nearEqual(new Vector(2, 3), tol));
-  assertTrue(at.transform(1, 0).nearEqual(new Vector(3, 3), tol));
-  assertTrue(at.transform(Vector.EAST).nearEqual(new Vector(3, 3), tol));
-  assertTrue(at.transform(0, 1).nearEqual(new Vector(2, 4), tol));
-  assertTrue(at.transform(Vector.NORTH).nearEqual(new Vector(2, 4), tol));
+  const a2 = AffineTransform.IDENTITY.translate(2, 3);
+  assertTrue(a2.transform(0, 0).nearEqual(new Vector(2, 3), tol));
+  assertTrue(a2.transform(Vector.ORIGIN).nearEqual(new Vector(2, 3), tol));
+  assertTrue(a2.transform(1, 0).nearEqual(new Vector(3, 3), tol));
+  assertTrue(a2.transform(Vector.EAST).nearEqual(new Vector(3, 3), tol));
+  assertTrue(a2.transform(0, 1).nearEqual(new Vector(2, 4), tol));
+  assertTrue(a2.transform(Vector.NORTH).nearEqual(new Vector(2, 4), tol));
 
   // translate with Vector
-  at = AffineTransform.IDENTITY.translate(new MutableVector(2, 3));
-  assertTrue(at.transform(0, 0).nearEqual(new Vector(2, 3), tol));
-  assertTrue(at.transform(Vector.ORIGIN).nearEqual(new Vector(2, 3), tol));
-  assertTrue(at.transform(1, 0).nearEqual(new Vector(3, 3), tol));
-  assertTrue(at.transform(Vector.EAST).nearEqual(new Vector(3, 3), tol));
-  assertTrue(at.transform(new MutableVector(0, 1)).nearEqual(new Vector(2, 4), tol));
-  assertTrue(at.transform(Vector.NORTH).nearEqual(new Vector(2, 4), tol));
+  const a3 = AffineTransform.IDENTITY.translate(new MutableVector(2, 3));
+  assertTrue(a3.transform(0, 0).nearEqual(new Vector(2, 3), tol));
+  assertTrue(a3.transform(Vector.ORIGIN).nearEqual(new Vector(2, 3), tol));
+  assertTrue(a3.transform(1, 0).nearEqual(new Vector(3, 3), tol));
+  assertTrue(a3.transform(Vector.EAST).nearEqual(new Vector(3, 3), tol));
+  assertTrue(a3.transform(new MutableVector(0, 1)).nearEqual(new Vector(2, 4), tol));
+  assertTrue(a3.transform(Vector.NORTH).nearEqual(new Vector(2, 4), tol));
 
   // scale
-  at = AffineTransform.IDENTITY.scale(2, 3);
-  assertTrue(at.transform(Vector.ORIGIN).nearEqual(Vector.ORIGIN, tol));
-  assertTrue(at.transform(Vector.NORTH).nearEqual(new Vector(0, 3), tol));
-  assertTrue(at.transform(Vector.EAST).nearEqual(new Vector(2, 0), tol));
-  assertTrue(at.transform(new Vector(1, 1)).nearEqual(new Vector(2, 3), tol));
+  const a4 = AffineTransform.IDENTITY.scale(2, 3);
+  assertTrue(a4.transform(Vector.ORIGIN).nearEqual(Vector.ORIGIN, tol));
+  assertTrue(a4.transform(Vector.NORTH).nearEqual(new Vector(0, 3), tol));
+  assertTrue(a4.transform(Vector.EAST).nearEqual(new Vector(2, 0), tol));
+  assertTrue(a4.transform(new Vector(1, 1)).nearEqual(new Vector(2, 3), tol));
 
   // rotate
-  at = AffineTransform.IDENTITY.rotate(Math.PI/2);
-  assertTrue(at.transform(Vector.ORIGIN).nearEqual(Vector.ORIGIN, tol));
-  assertTrue(at.transform(Vector.EAST).nearEqual(Vector.NORTH, tol));
-  assertTrue(at.transform(Vector.NORTH).nearEqual(Vector.WEST, tol));
-  assertTrue(at.transform(Vector.WEST).nearEqual(Vector.SOUTH, tol));
-  assertTrue(at.transform(Vector.SOUTH).nearEqual(Vector.EAST, tol));
+  const a5 = AffineTransform.IDENTITY.rotate(Math.PI/2);
+  assertTrue(a5.transform(Vector.ORIGIN).nearEqual(Vector.ORIGIN, tol));
+  assertTrue(a5.transform(Vector.EAST).nearEqual(Vector.NORTH, tol));
+  assertTrue(a5.transform(Vector.NORTH).nearEqual(Vector.WEST, tol));
+  assertTrue(a5.transform(Vector.WEST).nearEqual(Vector.SOUTH, tol));
+  assertTrue(a5.transform(Vector.SOUTH).nearEqual(Vector.EAST, tol));
 
   // concatenate:  translate followed by rotate
-  var rot = AffineTransform.IDENTITY.rotate(Math.PI/2);
-  var xlt = AffineTransform.IDENTITY.translate(2, 3);
-  var at3 = rot.concatenate(xlt);
+  const rot = AffineTransform.IDENTITY.rotate(Math.PI/2);
+  const xlt = AffineTransform.IDENTITY.translate(2, 3);
+  const at3 = rot.concatenate(xlt);
   assertTrue(at3.transform(Vector.EAST).nearEqual(
       rot.transform(xlt.transform(Vector.EAST)), tol));
   assertTrue(at3.transform(Vector.EAST).nearEqual(new Vector(-3, 3), tol));
 
   // concatenate:  rotate followed by translate
-  at3 = xlt.concatenate(rot);
-  assertTrue(at3.transform(Vector.EAST).nearEqual(
+  const at4 = xlt.concatenate(rot);
+  assertTrue(at4.transform(Vector.EAST).nearEqual(
       xlt.transform(rot.transform(Vector.EAST)), tol));
-  assertTrue(at3.transform(Vector.EAST).nearEqual(new Vector(2, 4), tol));
+  assertTrue(at4.transform(Vector.EAST).nearEqual(new Vector(2, 4), tol));
 
   // these methods require 2 numbers or a GenericVector
-  var at4 = new AffineTransform(1, 0, 0, 1, 0, 0);
-  assertThrows(() => at4.transform(0));
-  //assertThrows(() => at4.transform('foo'));
-  assertThrows(() => at4.translate(0));
-  //assertThrows(() => at4.translate('foo'));
+  const at5 = new AffineTransform(1, 0, 0, 1, 0, 0);
+  assertThrows(() => at5.transform(0));
+  assertThrows(() => at5.translate(0));
 };
 
 } // end class
