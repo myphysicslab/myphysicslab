db6a68454dc8424978b647f3750bd27c1b8ad993
Erik R. Neumann <erikn@myPhysicsLab.com>
2023-03-12 09:57:36 -0700

    VarsHistory has more options
    In VarsHistory add setNumberFormat() and setSeparator() methods. The output() method now
    prints variable names, and takes a parameter whether to print localized names. The default
    separator is now tab. Improved the documentation.

--- src/lab/graph/VarsHistory.js
+++ src/lab/graph/VarsHistory.js
@@ -22,39 +22,54 @@ const Memorizable = goog.require('myphysicslab.lab.util.Memorizable');
 const Util = goog.require('myphysicslab.lab.util.Util');
 const VarsList = goog.require('myphysicslab.lab.model.VarsList');
 
-/** Collects data from a {@link VarsList},
-storing it in a {@link HistoryList}. Each entry in
-the HistoryList is an array of numbers (a 'data sample') which represents the value of
-the variables at a point in time.
+/** Collects data from a {@link VarsList} and provides ways to print or access that data.
+The data is stored in a {@link HistoryList} with each entry being an array of numbers (a
+'data sample') representing the value of the variables at a point in time.
 
-To gather data a VarsHistory must be connected to a
-{@link myphysicslab.lab.util.MemoList} such as {@link myphysicslab.lab.app.SimRunner}.
-In most myPhysicsLab apps the following commands in
-{@link myphysicslab.lab.util.Terminal} will do this:
+The data is stored whenever {@link #memorize} is called. As a {@link Memorizable} the
+VarsHistory can be connected to a {@link myphysicslab.lab.util.MemoList} such as
+{@link myphysicslab.lab.app.SimRunner}. In most myPhysicsLab apps the following commands
+entered in {@link myphysicslab.lab.util.Terminal} will set this up:
 
     var hist = new VarsHistory(sim.getVarsList());
     simRun.addMemo(hist);
 
-After the simulation run is completed, print the data with this command in Terminal
+To memorize the starting initial conditions, call the memorize function once before starting the simulation.
+
+    hist.memorize()
+
+Run the simulation as long as desired. After the simulation run is completed, print the data:
 
     hist.output()
 
-See {@link #output}. The resulting text can then be copied from the Terminal text box,
-saved as a text document with suffix '.csv' (for 'comma separated values') and imported
-into a spreadsheet program for analysis.
+The resulting text can then be copied from the Terminal text box, saved as a text
+document and imported into a spreadsheet program or graphed with a Python script.
 
-The VarsHistory will gather data on all the variables by default. To change which
-variables are sampled or the order of the variables within each sample see
-{@link #setVariables}. For example:
+The default separator between numbers is the tab character. To instead use comma
+separated values:
 
-    hist.setVariables([3,0,1])
+    hist.setSeparator(', ');
+
+To change which variables are sampled or the order of the variables within each sample
+use {@link #setVariables}. For example:
+
+    hist.setVariables([9,0,1,2,3])
+
+The default format for printing numbers gives 5 decimal places, but if the number is
+too small then switches to exponential format. Use {@link #setNumberFormat} to change
+the formatting function. For example:
+
+    hist.setNumberFormat((n) => n.toFixed(2));
+
+That example uses an [arrow function](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
+but you can provide any function that takes one numeric argument and returns a string.
 
 * @implements {Memorizable}
 */
 class VarsHistory {
 /**
 * @param {!VarsList} variablesList the VarsList to gather data from
-* @param {number=} opt_capacity number of data samples to store
+* @param {number=} opt_capacity number of data samples to store, default is 100000
 */
 constructor(variablesList, opt_capacity) {
   /** @type {!VarsList}
@@ -76,15 +91,16 @@ constructor(variablesList, opt_capacity) {
   */
   this.numberFormat = Util.NF5E;
   /** separator between numbers
-  * @type {string}
+  * @type {!string}
   */
-  this.separator = ', ';
+  this.separator = '\t';
 };
 
 /** @override */
 toString() {
   return Util.ADVANCED ? '' : this.toStringShort().slice(0, -1)
       +', samples: '+this.dataPoints_.getSize()
+      +', separator: '+this.separator
       +', varIndex_: ['+Util.array2string(this.varIndex_, Util.NF0)
       +']}';
 };
@@ -124,11 +140,24 @@ memorize() {
 /** Returns string form of the data points. One line for each sample, using the
 number formatting function and text separator specified by the properties
 {@link #numberFormat} and {@link #separator}.
+* @param {boolean=} opt_localized `true` means print the localized versions of the
+*    variable names; `false` means print the language independent variable names;
+*    default is `true`
 * @return {string}
 */
-output() {
-  const iter = this.dataPoints_.getIterator();
+output(opt_localized) {
+  if (opt_localized===undefined) {
+    opt_localized = true;
+  }
   let s = '';
+  // output the variable names
+  this.varIndex_.forEach((idx, i) => {
+    s += (i > 0 ? this.separator : '');
+    s += this.variablesList_.getVariable(idx).getName(opt_localized);
+  });
+  s += '\n';
+  // output the variable numeric data
+  const iter = this.dataPoints_.getIterator();
   while (iter.hasNext()) {
     const data = iter.nextValue();
     s += Util.array2string(data, this.numberFormat, this.separator) + '\n';
@@ -143,19 +172,35 @@ reset() {
   this.dataPoints_.reset();
 };
 
-/** Sets the array of variable index numbers specifying which variables to remember.
+/** Sets the array of variable index numbers specifying which variables to memorize.
 @param {!Array<number>} varIndex array of variable index numbers specifying which
-    variables to remember
+    variables to memorize
 */
 setVariables(varIndex) {
   const numVars = this.variablesList_.numVariables();
   varIndex.forEach(idx => {
-    if (idx < 0 || idx > numVars) {
-      throw 'variable index '+idx+' not between 0 and '+numVars;
+    if (idx < 0 || idx > numVars - 1) {
+      throw 'variable index '+idx+' not between 0 and '+(numVars-1);
     }
   });
   this.varIndex_ = varIndex;
   this.dataPoints_.reset();
+};
+
+/**
+* @param {function(number): string} numberFormatFn
+*      the number formatting function; takes one numeric argument, returns a string.
+*/
+setNumberFormat(numberFormatFn) {
+  this.numberFormat = numberFormatFn;
+};
+
+/** Set the separator string to print between numbers
+* @param {!string} separator the separator string to print between numbers.
+*      Default is a comma and space.
+*/
+setSeparator(separator) {
+  this.separator = separator;
 };
 
 /** Returns arrays of the data points, one array for each sample.
